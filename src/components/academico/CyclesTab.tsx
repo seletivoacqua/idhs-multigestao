0f7a96a1cfa179c1b8efe19eea6d4669e5b6edeb
seletivoacqua <seletivoinstitutoacqua@gmail.com>
2026-03-05 23:34:54 -0300

    Update CyclesTab.tsx

--- src/components/academico/CyclesTab.tsx
+++ src/components/academico/CyclesTab.tsx
@@ -4,7 +4,18 @@ import { supabase } from '../../lib/supabase';
 import { useAuth } from '../../contexts/AuthContext';
 import { CertificateModal } from './CertificateModal';
 import { CertificateModalEAD } from './CertificateModalEAD';
-import { formatDateToDisplay, forceDateToDisplay, formatDateToDatabase, extractDatePart, formatDateInput, isDateGreaterOrEqual } from '../../utils/dateUtils';
+import { 
+  formatDateToDisplay, 
+  forceDateToDisplay, 
+  formatDateToDatabase, 
+  extractDatePart, 
+  isDateGreaterOrEqual,
+  formatDateInput,
+  parseDateInput,
+  formatDateForInput,
+  isValidDate,
+  compareDates
+} from '../../utils/dateUtils';
 
 interface Cycle {
   id: string;
